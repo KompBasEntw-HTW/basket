07bf8ecffe77302f5d8e305b975e62f86e1141f1
Vincent Dlugosch <dlugoschvincent@gmail.com>
2022-12-13 23:00:53 +0100

    feat: initialize basket service and add simple basket that can accept products

---
+++ src/main/java/de/extremecoffee/BasketController.java
@@ -0,0 +1,68 @@
+package de.extremecoffee;
+
+import java.util.UUID;
+
+import org.jboss.resteasy.reactive.RestQuery;
+
+import jakarta.transaction.Transactional;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.NotFoundException;
+import jakarta.ws.rs.POST;
+import jakarta.ws.rs.Path;
+import de.extremecoffee.basket.Basket;
+import de.extremecoffee.basket.BasketItem;
+import de.extremecoffee.basket.Item;
+import de.extremecoffee.basket.ItemId;
+
+@Path("/basket")
+public class BasketController {
+  @POST
+  @Transactional
+  @Path("/createEphemeral")
+  public UUID createEphemeral() {
+    var basket = new Basket();
+    basket.persist();
+    return basket.id;
+  }
+
+  @POST
+  @Transactional
+  @Path("/{basketId}/update")
+  public Basket update(ItemId itemId, @RestQuery Integer quantity, UUID basketId) {
+    Basket basket = Basket.findById(basketId);
+    Item item = Item.findById(itemId);
+
+    if (basket == null) {
+      throw new NotFoundException();
+    }
+
+    if (item == null) {
+      // TODO throw not found exception syncronize with products service
+      item = new Item();
+      item.productId = itemId.productId;
+      item.bagSizeId = itemId.bagSizeId;
+      item.persist();
+    }
+
+    for (var basketItem : basket.basketItems) {
+      if (basketItem.item.equals(item)) {
+        basketItem.quantity = quantity;
+        return basket;
+      }
+    }
+
+    BasketItem basketItem = new BasketItem();
+    basketItem.quantity = quantity;
+    basketItem.item = item;
+    basketItem.basket = basket;
+    basketItem.persist();
+    basket.basketItems.add(basketItem);
+    return basket;
+  }
+
+  @GET
+  @Path("/{id}")
+  public Basket getById(UUID id) {
+    return Basket.findById(id);
+  }
+}
---
+++ src/main/java/de/extremecoffee/basket/BasketItem.java
@@ -0,0 +1,33 @@
+package de.extremecoffee.basket;
+
+import org.eclipse.microprofile.openapi.annotations.media.Schema;
+
+import io.quarkus.hibernate.orm.panache.PanacheEntityBase;
+import jakarta.json.bind.annotation.JsonbTransient;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinColumns;
+import jakarta.persistence.ManyToOne;
+
+@Entity
+public class BasketItem extends PanacheEntityBase {
+  @Id
+  @GeneratedValue
+  @Schema(readOnly = true)
+  public Long id;
+
+  @ManyToOne
+  @JoinColumns({
+      @JoinColumn(name = "bagsizeid"), @JoinColumn(name = "productid")
+  })
+  public Item item;
+
+  @JsonbTransient
+  @ManyToOne
+  @JoinColumn(name = "basket_id")
+  public Basket basket;
+
+  public Integer quantity;
+}
