ea47bbf695a16e61c7fb71ebf4d02977ce1457f1
Vincent Dlugosch <dlugoschvincent@gmail.com>
2023-07-19 17:10:01 +0300

    fix: add equals method to id class

--- src/main/java/de/extremecoffee/basket/ItemId.java
+++ src/main/java/de/extremecoffee/basket/ItemId.java
@@ -6,4 +6,13 @@ public class ItemId implements Serializable {
   public Long bagSizeId;
 
   public Long productId;
+
+  @Override
+  public boolean equals(Object o) {
+    Item item = (Item) o;
+    if (bagSizeId == this.bagSizeId && productId == this.productId) {
+      return true;
+    }
+    return false;
+  }
 }
