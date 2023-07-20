ce13f8751678b3315880e6edffcdd4b858ed0488
Vincent Dlugosch <dlugoschvincent@gmail.com>
2023-07-20 14:51:28 +0200

    fix: equals method

--- src/main/java/de/extremecoffee/basket/Item.java
+++ src/main/java/de/extremecoffee/basket/Item.java
@@ -8,20 +8,11 @@ import jakarta.persistence.IdClass;
 @Entity
 @IdClass(ItemId.class)
 public class Item extends PanacheEntityBase {
-  @Id
-  public Long bagSizeId;
+	@Id
+	public Long bagSizeId;
 
-  @Id
-  public Long productId;
+	@Id
+	public Long productId;
 
-  public Integer quantityInStock;
-
-  @Override
-  public boolean equals(Object o) {
-    Item item = (Item) o;
-    if (item.bagSizeId == this.bagSizeId && item.productId == this.productId) {
-      return true;
-    }
-    return false;
-  }
+	public Integer quantityInStock;
 }
--- src/main/java/de/extremecoffee/basket/ItemId.java
+++ src/main/java/de/extremecoffee/basket/ItemId.java
@@ -3,16 +3,16 @@ package de.extremecoffee.basket;
 import java.io.Serializable;
 
 public class ItemId implements Serializable {
-  public Long bagSizeId;
+	public Long bagSizeId;
 
-  public Long productId;
+	public Long productId;
 
-  @Override
-  public boolean equals(Object o) {
-    Item item = (Item) o;
-    if (item.bagSizeId == bagSizeId && item.productId == productId) {
-      return true;
-    }
-    return false;
-  }
+	@Override
+	public boolean equals(Object o) {
+		ItemId itemId = (ItemId) o;
+		if (itemId.bagSizeId == this.bagSizeId && itemId.productId == this.productId) {
+			return true;
+		}
+		return false;
+	}
 }
